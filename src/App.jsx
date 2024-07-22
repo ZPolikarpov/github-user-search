2fb4334eaf32cfa220049678284e7a16bbfb0d40
Zakhar Polikarpov <zpolikarpov@veka.de>
2024-07-22 16:20:24 +0200

    Searchbar css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import icon_logo from "./assets/images/icon-website.svg"
 import icon_moon from "./assets/images/icon-moon.svg"
 import icon_search from "./assets/images/icon-search.svg"
 
@@ -8,13 +7,13 @@ function App() {
       <div className="container">
         <header className="site-header">
           <h1 className="heading-1" style={{fontSize: "1.5rem"}}>devfinder</h1>
-          <button className="button" data-type="accent" style={{letterSpacing: "2.5px", gap: "1rem"}}>DARK<img src={icon_moon} alt="Moon" /></button>
+          <button className="button" data-type="accent" style={{letterSpacing: "2.5px", gap: "1rem", fontSize: "0.813rem"}}>DARK<img src={icon_moon} alt="Moon" /></button>
         </header>
 
-        <main>
-          <div className="search-bar">
+        <main className="main">
+          <div className="search-bar | margin-block-start-8">
             <img src={icon_search} alt="Search" />
-            <input type="text" placeholder="Search GitHub username..."/>
+            <input type="text" placeholder="Search GitHub username…"/>
             <button className="button" data-type="primary">Search</button>
           </div>
 
